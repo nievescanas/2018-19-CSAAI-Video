299338b9f290da4bb14bc375d65653c7de9753a8
nievescanas <nievescanasmartin@gmail.com>
2019-04-06 15:58:48 +0200

    Constructor de videos

--- mouse-01.js
+++ mouse-01.js
@@ -1,22 +1,41 @@
+function video(name_video,width,height){
+  this.video = document.getElementById(name_video);
+  this.video.width = width;
+  this.video.height = height;
+  this.play = function(ubicación){
+    this.video.src = ubicación;
+  }
+}
+
 function main()
 {
-  console.log("Video 02...")
+  //-- Inicialización y construcción de los videos
 
-  //-- Obtener el elemento de video
-  video1 = document.getElementById("video1")
-
-  //-- Establecer el tamaño del vídeo
-  video1.width = 200;
-  video1.height= 100;
+  var video1 = new video('video1',200,100)
+  var video2 = new video('video2',200,100)
+  var video3 = new video('video3',200,100)
+  var video0 = new video('video0',600,300)
 
   //-- Boton de Play
-  ver = document.getElementById('play')
+  ver1 = document.getElementById('play1')
+  ver2 = document.getElementById('play2')
+  ver3 = document.getElementById('play3')
+
+
 
   //-- Al apretar el boton de ver video
-  ver.onclick = () => {
-    console.log("Ver!")
-
+  ver1.onclick = () => {
     //-- Indicar la fuente del vídeo
-    video1.src ="https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4"
+video0.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4")
+
+    video1.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4")
+  }
+  ver2.onclick = () => {
+    //-- Indicar la fuente del vídeo
+    video2.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4")
+  }
+  ver3.onclick = () => {
+    //-- Indicar la fuente del vídeo
+    video3.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4")
   }
 }
