f25a94da13da614cb512e3ff6286543d626d377c
nievescanas <nievescanasmartin@gmail.com>
2019-04-08 12:14:46 +0200

    Retoques de estilo

--- mouse-01.js
+++ mouse-01.js
@@ -1,3 +1,4 @@
+
 function video(name_video,width,height){
   this.video = document.getElementById(name_video);
   this.video.width = width;
@@ -26,8 +27,7 @@ function hora(segundos){
  return hora+":"+minuto+":"+segundo;
 }
 
-function main()
-{
+function main(){
   //-- Inicialización y construcción de los videos
   var video0 = new video('video0',600,300)
   var video1 = new video('video1',200,100)
@@ -44,56 +44,58 @@ function main()
   time2 = document.getElementById('Time2')
   time3 = document.getElementById('Time3')
 
-  //-- Contador
-  setInterval(()=>{
-      time0.innerHTML = hora(video0.time());
-    },20);
-  setInterval(()=>{
-      time1.innerHTML = hora(video1.time());
-    },20);
-  setInterval(()=>{
-      time2.innerHTML = hora(video2.time());
-    },20);
-  setInterval(()=>{
-      time3.innerHTML = hora(video3.time());
-    },20);
+  play = document.getElementById('play')
+  play.onclick = () => {
+    document.getElementById('play').style.display = 'none';
+    //-- Contador
+    setInterval(()=>{
+        time0.innerHTML = hora(0);
+      },20);
+    setInterval(()=>{
+        time1.innerHTML = hora(video1.time());
+      },20);
+    setInterval(()=>{
+        time2.innerHTML = hora(video2.time());
+      },20);
+    setInterval(()=>{
+        time3.innerHTML = hora(video3.time());
+      },20);
 
+    //-- Play videos de selección
+    video1.play("Mb.mp4",0)
+    video2.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4",0)
+    video3.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4",0)
 
-  //-- Play videos de selección
-  video1.play("Mb.mp4",0)
-  video2.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4",0)
-  video3.play("https://gsyc.urjc.es/jmplaza/csaai/realizador-fuente1.mp4",0)
+    //-- Al apretar el boton de ver video
+    ver1.onclick = () => {
+      //-- Indicar la fuente del vídeo
+      video0.play(video1.ubicacion(),video1.time())
+    }
+    ver2.onclick = () => {
+      video0.play(video2.ubicacion(),video2.time())
+    }
+    ver3.onclick = () => {
+      video0.play(video3.ubicacion(),video3.time())
+    }
 
-
-  //-- Al apretar el boton de ver video
-  ver1.onclick = () => {
-    //-- Indicar la fuente del vídeo
-    video0.play(video1.ubicacion(),video1.time())
-  }
-  ver2.onclick = () => {
-    video0.play(video2.ubicacion(),video2.time())
-  }
-  ver3.onclick = () => {
-    video0.play(video3.ubicacion(),video3.time())
-  }
-
-  //-- Salida de audio
-  ver1.onmouseover = () => {
-    video1.audio(false);
-  }
-  ver1.onmouseout = () => {
-    video1.audio(true);
-  }
-  ver2.onmouseover = () => {
-    video2.audio(false);
-  }
-  ver2.onmouseout = () => {
-    video2.audio(true);
-  }
-  ver3.onmouseover = () => {
-    video3.audio(false);
-  }
-  ver3.onmouseout = () => {
-    video3.audio(true);
+    //-- Salida de audio
+    ver1.onmouseover = () => {
+      video1.audio(false);
+    }
+    ver1.onmouseout = () => {
+      video1.audio(true);
+    }
+    ver2.onmouseover = () => {
+      video2.audio(false);
+    }
+    ver2.onmouseout = () => {
+      video2.audio(true);
+    }
+    ver3.onmouseover = () => {
+      video3.audio(false);
+    }
+    ver3.onmouseout = () => {
+      video3.audio(true);
+    }
   }
 }
